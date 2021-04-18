6d9d0caf9f8c939de0dd7530ec9cac0d1ad97c96
Li Gao <ligao0807@gmail.com>
2021-04-17 23:31:10 -0400

    not work yet

--- zip_code_search/src/App.js
+++ zip_code_search/src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 // eslint-disable-next-line
-import { render } from 'react-dom';
+// import axios from 'axios';
 
 
 //creates array for api data, an open string for City and state is set to false until found
@@ -18,13 +18,50 @@ class searchAPI extends Component {
   handleInputChange = (event) => {
     this.setState({ Zip: event.target.value});
   }
+
 //function for what happens when you click the search button
-  handleSearchClick = async () => {
+  handleSearchClick = async() => {
     let ZipCode = this.state.Zip;
     // eslint-disable-next-line
-    let linkToAPI = 'https://ctp-zip-api.herokuapp.com/zip/:zipcode' + ZipCode;
-    console.log(this.state.Zip);
+    let linkToAPI = 'https://ctp-zip-api.herokuapp.com/zip/' + ZipCode;
+  // console.log(this.state.Zip);
+    fetch(linkToAPI)
+    .then((response) => {
+      if(response.status === 404){
+          return;
+        }
+      return response.json();
+  })
+    .then((data) => { //data is response.json()
+      this.setState({apiData: data});
+    })
+  .catch((error) => {
+      console.error('Error:', error);
+  });
   }
+
+  makeTable = () => {
+    let currData = this.state.apiData;
+    let foundMatch = this.state.found;
+    let table = [];
+    //found is false when we get 404 error
+    if(!foundMatch){
+        table.push(<tr key={-1}><td>No Results</td></tr>);
+        return table;
+    } else {
+        let state = currData.State;
+        let population = currData.EstimatedPopulation
+        let totalwages = currData.TotalWages;
+        table.push(
+          <tr key={currData.id}>
+            <td>State: {state}</td>
+            <td>Total Wages: {totalwages}</td>
+            <td>Estimated Population: {population}</td>
+          </tr>
+        );
+        return table;
+    }
+}   
 
 
   render() {
@@ -32,9 +69,15 @@ class searchAPI extends Component {
       <div className="App">
         <div className="App-header">
           <h3>Search Zip: </h3>
-          <input type="text" placeholder="zip code" onChange={this.handleInputChange}/>
+          <input type="text" placeholder="zip code"  onChange={this.handleInputChange}/>
           <button className="search zip" onClick={this.handleSearchClick}>Search</button>
         </div>
+        <br/>
+                <table id="data">
+                    <tbody>
+                    {this.makeTable()}
+                    </tbody>
+                </table>
       </div>
     );
   }
